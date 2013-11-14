522857316945c9ff6770eb028940815d3849ded0
Carlos Guisao Franco <fguisao@altavistaed.com>
2013-11-14 09:21:34 -0500

    Remove coffee mode old version.

--- elpa/coffee-mode-0.4.1/coffee-mode-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; coffee-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (20984 7241
-;;;;;;  132314 225000))
-;;; Generated autoloads from coffee-mode.el
-
-(autoload 'coffee-mode "coffee-mode" "\
-Major mode for editing CoffeeScript.
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-
-;;;***
-
-;;;### (autoloads nil nil ("coffee-mode-pkg.el") (20984 7241 144560
-;;;;;;  1000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; coffee-mode-autoloads.el ends here
