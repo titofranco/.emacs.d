35f8da29f23fe3e282114edb41479d3098a77c8d
Carlos Guisao Franco <fguisao@altavistaed.com>
2013-11-14 09:17:47 -0500

    Fix js2-mode call.

--- init.el
+++ init.el
@@ -51,6 +51,7 @@
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 
 ;; js2-mode from https://github.com/mooz/js2-mode
+(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\.erb$" . rhtml-mode))
 ;; JS indentation
