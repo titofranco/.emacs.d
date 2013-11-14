dfa3cd975d8e7d5c66a107fe508d39226fac10b3
Carlos Guisao Franco <fguisao@altavistaed.com>
2013-11-14 09:17:00 -0500

    Update js2 mode.

--- elpa/js2-mode-20130619/js2-mode-autoloads.el
+++ elpa/js2-mode-20130619/js2-mode-autoloads.el
@@ -3,8 +3,24 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "js2-mode" "js2-mode.el" (20984 6442 892349
-;;;;;;  813000))
+;;;### (autoloads (js2-imenu-extras-mode js2-imenu-extras-setup)
+;;;;;;  "js2-imenu-extras" "js2-imenu-extras.el" (21115 35737))
+;;; Generated autoloads from js2-imenu-extras.el
+
+(autoload 'js2-imenu-extras-setup "js2-imenu-extras" "\
+
+
+\(fn)" nil nil)
+
+(autoload 'js2-imenu-extras-mode "js2-imenu-extras" "\
+Toggle Imenu support for frameworks and structural patterns.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads (js2-mode js2-minor-mode) "js2-mode" "js2-mode.el"
+;;;;;;  (21115 35737))
 ;;; Generated autoloads from js2-mode.el
 
 (autoload 'js2-minor-mode "js2-mode" "\
@@ -19,6 +35,10 @@ highlighting features of `js2-mode'.
 Major mode for editing JavaScript code.
 
 \(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("js2-mode-pkg.el") (21115 35737 422114))
 
 ;;;***
 
